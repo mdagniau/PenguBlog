c89a93ea41bff0b344c208739746de463899562a
mdagniau <m.dagniau@orange.fr>
2015-10-17 22:38:01 +0200

    Pg & design
    Connection with postgres ok
    Creation tables ok except for tables with foreign key
    Design "imotep imotep" =
    * ng repeat for number of articles in database ok
    * merdouille for open addComment in function of article selectionned

--- mysql.js
+++
@@ -1,9 +0,0 @@
-var mysql = require('mysql');
-
-var mySqlClient = mysql.createConnection({
-  host     : "localhost",
-  user     : "pengu",
-  password : "pengu",
-  database : "pengu"
-});
-
--- public/js/app.js
+++ public/js/app.js
@@ -2,48 +2,150 @@
 
 var blog = angular.module('blog', []);
 var user;
+var userConnected = true;
+
 
 blog.controller('blogCtrl', ['$scope', function ($scope) {
+    $scope.showArticle = true;
+    $scope.editVar = true;
+
+    $scope.numberArticles = 5;
+    $scope.getNumber = function(num) {
+      return new Array(num);   
+    }
+
     $scope.login = function (){
       $scope.showLogin = true;
+      $scope.showSignup = false;
+      $scope.showArticle = false;
+      $scope.commentOn = false;
+      $scope.showAccount = false;
+      $scope.newArticle = false;
+      $scope.settings = false;
     }
     $scope.signup = function (){
+      $scope.showLogin = false;
       $scope.showSignup = true;
+      $scope.showArticle = false;
+      $scope.commentOn = false;
+      $scope.showAccount = false;
+      $scope.newArticle = false;
+      $scope.settings = false;
     }
-    $scope.openCommentForm = function(){
-      $scope.commentOn = true;
+    $scope.myAccount = function (){
+      $scope.showLogin = false;
+      $scope.showSignup = false;
+      $scope.showArticle = false;
+      $scope.commentOn = false;
+      $scope.showAccount = true;
+      $scope.newArticle = false;
+      $scope.settings = false;
     }
-}]);
+    $scope.editAccount = function(){
+      $scope.editVar = false;
+    }
+    $scope.addAnArticle = function (){
+      $scope.showLogin = false;
+      $scope.showSignup = false;
+      $scope.showArticle = false;
+      $scope.commentOn = false;
+      $scope.showAccount = false;
+      $scope.newArticle = true;
+      $scope.settings = false;
+    }
+    $scope.managePublications = function (){
+      $scope.showLogin = false;
+      $scope.showSignup = false;
+      $scope.showArticle = false;
+      $scope.commentOn = false;
+      $scope.showAccount = false;
+      $scope.newArticle = false;
+      $scope.settings = true;
+    }
+    $scope.openCommentForm = function(index){
+      $scope.showLogin = false;
+      $scope.showSignup = false;
+      $scope.showArticle = true;
+      alert(index);
+      /*var varShow = 'commentOn'+index;
+      $scope.varShow = true;*/
+      var nameElement = "addComment"+index;
+      document.getElementById(nameElement).setAttribute('ng-show', true);
+      debugger;
+      $scope.showAccount = false;
+      $scope.newArticle = false;
+      $scope.settings = false;
+    }
 
-blog.controller('loginCtrl', ['$scope', function ($scope) {
+
+
+    /*******************************************
+    * GENERIC METHODS
+    ********************************************/
+
+    $scope.submitDialog = function(varShow, object){
+        object = angular.copy(object);
+        $scope.varShow = false;
+    }
+
+    $scope.cancelDialog = function(varShow){
+        $scope.varShow = false;
+        $scope.showArticle = true;
+    }
+
+
+    /*******************************************
+    * METHODS NO GENERIC
+    ********************************************/
+
     $scope.submitLoginForm = function(user){
        user = angular.copy(user);
        $scope.showLogin = false;
     }
     $scope.cancelLogin = function (){
       $scope.showLogin = false;
+      $scope.showArticle = true;
     }
-}]);
 
-blog.controller('signupCtrl', ['$scope', function ($scope) {
     $scope.submitSignupForm = function(newUser){
        user = angular.copy(newUser);
        $scope.showSignup = false;
     }
     $scope.cancelSignup = function (){
       $scope.showSignup = false;
+      $scope.showArticle = true;
     }
-}]);
 
-blog.controller('commenttrl', ['$scope', function ($scope) {
     $scope.sendComment = function(newComment){
        comment = angular.copy(newComment);
        $scope.commentOn = false;
     }
-    $scope.cancelSignup = function (){
+    $scope.cancelComment = function (){
       $scope.commentOn = false;
+      $scope.showArticle = true;
+    }
+    $scope.cancelMyAccount = function (){
+      $scope.showAccount = false;
+      $scope.editVar = true;
+      $scope.showArticle = true;
+    }
+
+}]);
+
+blog.controller('articleCtrl', ['$scope', function ($scope) {
+    $scope.addFavorite = function(index){
+      var nameElement = "favoriteIcon"+index;
+      var element = document.getElementById(nameElement);
+      var source = element.getAttribute("src");
+      if(source == "public/img/icon/heart-outline.png"){        
+       element.src = "public/img/icon/heart.png" ;
+      }
+      else{
+        element.src = "public/img/icon/heart-outline.png" ;
+      }
     }
 }]);
+
 
 
 blog.controller('helloCtrl', ['$scope', function ($scope) {
